052bf92ced831abe053f81db3f0ca2c8b3fd8eb0
20urc3 <94982366+20urc3@users.noreply.github.com>
2024-12-10 15:38:54 +0100

    Update domato.py

--- docs/domato.py
+++ docs/domato.py
@@ -1537,3 +1537,401 @@ class Grammar(object):
     def _get_any_var(self, context):
         var_type = random.choice(list(context['variables'].keys()))
         return random.choice(context['variables'][var_type])
+
+#   Domato - HTML types
+#   --------------------------------------
+#
+#   Written and maintained by Ivan Fratric <ifratric@google.com>
+#
+#   Copyright 2017 Google Inc. All Rights Reserved.
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+# A map from tag name to corresponding type for HTML tags
+_HTML_TYPES = {
+    'a': 'HTMLAnchorElement',
+    'abbr': 'HTMLUnknownElement',
+    'acronym': 'HTMLUnknownElement',
+    'address': 'HTMLUnknownElement',
+    'applet': 'HTMLUnknownElement',
+    'area': 'HTMLAreaElement',
+    'article': 'HTMLUnknownElement',
+    'aside': 'HTMLUnknownElement',
+    'audio': 'HTMLAudioElement',
+    'b': 'HTMLUnknownElement',
+    'base': 'HTMLBaseElement',
+    'basefont': 'HTMLUnknownElement',
+    'bdi': 'HTMLUnknownElement',
+    'bdo': 'HTMLUnknownElement',
+    'bgsound': 'HTMLUnknownElement',
+    'big': 'HTMLUnknownElement',
+    'blockquote': 'HTMLUnknownElement',
+    'br': 'HTMLBRElement',
+    'button': 'HTMLButtonElement',
+    'canvas': 'HTMLCanvasElement',
+    'caption': 'HTMLTableCaptionElement',
+    'center': 'HTMLUnknownElement',
+    'cite': 'HTMLUnknownElement',
+    'code': 'HTMLUnknownElement',
+    'col': 'HTMLTableColElement',
+    'colgroup': 'HTMLUnknownElement',
+    'command': 'HTMLUnknownElement',
+    'content': 'HTMLContentElement',
+    'data': 'HTMLDataElement',
+    'datalist': 'HTMLDataListElement',
+    'dd': 'HTMLUnknownElement',
+    'del': 'HTMLModElement',
+    'details': 'HTMLDetailsElement',
+    'dfn': 'HTMLUnknownElement',
+    'dialog': 'HTMLDialogElement',
+    'dir': 'HTMLDirectoryElement',
+    'div': 'HTMLDivElement',
+    'dl': 'HTMLDListElement',
+    'dt': 'HTMLUnknownElement',
+    'em': 'HTMLUnknownElement',
+    'embed': 'HTMLEmbedElement',
+    'fieldset': 'HTMLFieldSetElement',
+    'figcaption': 'HTMLUnknownElement',
+    'figure': 'HTMLUnknownElement',
+    'font': 'HTMLFontElement',
+    'footer': 'HTMLUnknownElement',
+    'form': 'HTMLFormElement',
+    'frame': 'HTMLFrameElement',
+    'frameset': 'HTMLFrameSetElement',
+    'h1': 'HTMLHeadingElement',
+    'h2': 'HTMLHeadingElement',
+    'h3': 'HTMLHeadingElement',
+    'h4': 'HTMLHeadingElement',
+    'h5': 'HTMLHeadingElement',
+    'h6': 'HTMLHeadingElement',
+    'header': 'HTMLUnknownElement',
+    'hgroup': 'HTMLUnknownElement',
+    'hr': 'HTMLHRElement',
+    'i': 'HTMLUnknownElement',
+    'iframe': 'HTMLIFrameElement',
+    'image': 'HTMLImageElement',
+    'img': 'HTMLImageElement',
+    'input': 'HTMLInputElement',
+    'ins': 'HTMLModElement',
+    'isindex': 'HTMLUnknownElement',
+    'kbd': 'HTMLUnknownElement',
+    'keygen': 'HTMLKeygenElement',
+    'label': 'HTMLLabelElement',
+    'layer': 'HTMLUnknownElement',
+    'legend': 'HTMLLegendElement',
+    'li': 'HTMLLIElement',
+    'link': 'HTMLLinkElement',
+    'listing': 'HTMLUnknownElement',
+    'main': 'HTMLUnknownElement',
+    'map': 'HTMLMapElement',
+    'mark': 'HTMLUnknownElement',
+    'marquee': 'HTMLMarqueeElement',
+    'menu': 'HTMLMenuElement',
+    'menuitem': 'HTMLMenuItemElement',
+    'meta': 'HTMLMetaElement',
+    'meter': 'HTMLMeterElement',
+    'nav': 'HTMLUnknownElement',
+    'nobr': 'HTMLUnknownElement',
+    'noembed': 'HTMLUnknownElement',
+    'noframes': 'HTMLUnknownElement',
+    'nolayer': 'HTMLUnknownElement',
+    'noscript': 'HTMLUnknownElement',
+    'object': 'HTMLObjectElement',
+    'ol': 'HTMLOListElement',
+    'optgroup': 'HTMLOptGroupElement',
+    'option': 'HTMLOptionElement',
+    'output': 'HTMLOutputElement',
+    'p': 'HTMLParagraphElement',
+    'param': 'HTMLParamElement',
+    'picture': 'HTMLPictureElement',
+    'plaintext': 'HTMLUnknownElement',
+    'pre': 'HTMLPreElement',
+    'progress': 'HTMLProgressElement',
+    'q': 'HTMLQuoteElement',
+    'rp': 'HTMLUnknownElement',
+    'rt': 'HTMLUnknownElement',
+    'ruby': 'HTMLUnknownElement',
+    's': 'HTMLUnknownElement',
+    'samp': 'HTMLUnknownElement',
+    'section': 'HTMLUnknownElement',
+    'select': 'HTMLSelectElement',
+    'shadow': 'HTMLShadowElement',
+    'small': 'HTMLUnknownElement',
+    'source': 'HTMLSourceElement',
+    'span': 'HTMLSpanElement',
+    'strike': 'HTMLUnknownElement',
+    'strong': 'HTMLUnknownElement',
+    'style': 'HTMLStyleElement',
+    'sub': 'HTMLUnknownElement',
+    'summary': 'HTMLUnknownElement',
+    'sup': 'HTMLUnknownElement',
+    'table': 'HTMLTableElement',
+    'tbody': 'HTMLTableSectionElement',
+    'td': 'HTMLUnknownElement',
+    'template': 'HTMLTemplateElement',
+    'textarea': 'HTMLTextAreaElement',
+    'tfoot': 'HTMLTableSectionElement',
+    'th': 'HTMLTableCellElement',
+    'thead': 'HTMLTableSectionElement',
+    'time': 'HTMLTimeElement',
+    'title': 'HTMLTitleElement',
+    'tr': 'HTMLTableRowElement',
+    'track': 'HTMLTrackElement',
+    'tt': 'HTMLUnknownElement',
+    'u': 'HTMLUnknownElement',
+    'ul': 'HTMLUListElement',
+    'var': 'HTMLUnknownElement',
+    'video': 'HTMLVideoElement',
+    'wbr': 'HTMLUnknownElement',
+    'xmp': 'HTMLUnknownElement'
+}
+
+#   Domato - MathML types
+#   --------------------------------------
+#
+#   Written and maintained by Ivan Fratric <ifratric@google.com>
+#
+#   Copyright 2017 Google Inc. All Rights Reserved.
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+# A map from tag name to corresponding type for MathML tags
+_MATHML_TYPES = {
+    'annotation': 'MathMLElement',
+    'annotation-xml': 'MathMLElement',
+    'maction': 'MathMLElement',
+    'math': 'MathMLElement',
+    'merror': 'MathMLElement',
+    'mfrac': 'MathMLElement',
+    'mi': 'MathMLElement',
+    'mmultiscripts': 'MathMLElement',
+    'mn': 'MathMLElement',
+    'mo': 'MathMLElement',
+    'mover': 'MathMLElement',
+    'mpadded': 'MathMLElement',
+    'mphantom': 'MathMLElement',
+    'mprescripts': 'MathMLElement',
+    'mroot': 'MathMLElement',
+    'mrow': 'MathMLElement',
+    'ms': 'MathMLElement',
+    'mspace': 'MathMLElement',
+    'msqrt': 'MathMLElement',
+    'mstyle': 'MathMLElement',
+    'msub': 'MathMLElement',
+    'msubsup': 'MathMLElement',
+    'msup': 'MathMLElement',
+    'mtable': 'MathMLElement',
+    'mtd': 'MathMLElement',
+    'mtext': 'MathMLElement',
+    'mtr': 'MathMLElement',
+    'munder': 'MathMLElement',
+    'munderover': 'MathMLElement',
+    'none': 'MathMLElement',
+    'semantics': 'MathMLelement'
+}
+
+#   Domato - SVG types
+#   --------------------------------------
+#
+#   Written and maintained by Ivan Fratric <ifratric@google.com>
+#
+#   Copyright 2017 Google Inc. All Rights Reserved.
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+# A map from tag name to corresponding type for SVG tags
+_SVG_TYPES = {
+    'a': 'SVGAElement',
+    'altGlyph': 'SVGElement',
+    'altGlyphDef': 'SVGElement',
+    'altGlyphItem': 'SVGElement',
+    'animate': 'SVGAnimateElement',
+    'animateColor': 'SVGElement',
+    'animateMotion': 'SVGAnimateMotionElement',
+    'animateTransform': 'SVGAnimateTransformElement',
+    'circle': 'SVGCircleElement',
+    'clipPath': 'SVGClipPathElement',
+    'color-profile': 'SVGElement',
+    'cursor': 'SVGCursorElement',
+    'defs': 'SVGDefsElement',
+    'desc': 'SVGDescElement',
+    'discard': 'SVGDiscardElement',
+    'ellipse': 'SVGEllipseElement',
+    'feBlend': 'SVGFEBlendElement',
+    'feColorMatrix': 'SVGFEColorMatrixElement',
+    'feComponentTransfer': 'SVGFEComponentTransferElement',
+    'feComposite': 'SVGFECompositeElement',
+    'feConvolveMatrix': 'SVGFEConvolveMatrixElement',
+    'feDiffuseLighting': 'SVGFEDiffuseLightingElement',
+    'feDisplacementMap': 'SVGFEDisplacementMapElement',
+    'feDistantLight': 'SVGFEDistantLightElement',
+    'feDropShadow': 'SVGFEDropShadowElement',
+    'feFlood': 'SVGFEFloodElement',
+    'feFuncA': 'SVGFEFuncAElement',
+    'feFuncB': 'SVGFEFuncBElement',
+    'feFuncG': 'SVGFEFuncGElement',
+    'feFuncR': 'SVGFEFuncRElement',
+    'feGaussianBlur': 'SVGFEGaussianBlurElement',
+    'feImage': 'SVGFEImageElement',
+    'feMerge': 'SVGFEMergeElement',
+    'feMergeNode': 'SVGFEMergeNodeElement',
+    'feMorphology': 'SVGFEMorphologyElement',
+    'feOffset': 'SVGFEOffsetElement',
+    'fePointLight': 'SVGFEPointLightElement',
+    'feSpecularLighting': 'SVGFESpecularLightingElement',
+    'feSpotLight': 'SVGFESpotLightElement',
+    'feTile': 'SVGFETileElement',
+    'feTurbulence': 'SVGFETurbulenceElement',
+    'filter': 'SVGFilterElement',
+    'font': 'SVGElement',
+    'font-face': 'SVGElement',
+    'font-face-format': 'SVGElement',
+    'font-face-name': 'SVGElement',
+    'font-face-src': 'SVGElement',
+    'font-face-uri': 'SVGElement',
+    'foreignObject': 'SVGForeignObjectElement',
+    'g': 'SVGGElement',
+    'glyph': 'SVGElement',
+    'glyphRef': 'SVGElement',
+    'hatch': 'SVGElement',
+    'hatchpath': 'SVGElement',
+    'hkern': 'SVGElement',
+    'image': 'SVGImageElement',
+    'line': 'SVGLineElement',
+    'linearGradient': 'SVGLinearGradientElement',
+    'marker': 'SVGMarkerElement',
+    'mask': 'SVGMaskElement',
+    'mesh': 'SVGElement',
+    'meshgradient': 'SVGElement',
+    'meshpatch': 'SVGElement',
+    'meshrow': 'SVGElement',
+    'metadata': 'SVGMetadataElement',
+    'missing-glyph': 'SVGElement',
+    'mpath': 'SVGMPathElement',
+    'path': 'SVGPathElement',
+    'pattern': 'SVGPatternElement',
+    'polygon': 'SVGPolygonElement',
+    'polyline': 'SVGPolylineElement',
+    'radialGradient': 'SVGRadialGradientElement',
+    'rect': 'SVGRectElement',
+    'set': 'SVGSetElement',
+    'svg': 'SVGSVGElement',
+    'solidcolor': 'SVGElement',
+    'stop': 'SVGStopElement',
+    'switch': 'SVGSwitchElement',
+    'symbol': 'SVGSymbolElement',
+    'text': 'SVGTextElement',
+    'textPath': 'SVGTextPathElement',
+    'title': 'SVGTitleElement',
+    'tref': 'SVGElement',
+    'tspan': 'SVGTSpanElement',
+    'unknown': 'SVGElement',
+    'use': 'SVGUseElement',
+    'view': 'SVGViewElement',
+    'vkern': 'SVGElement'
+}
+
+<!-- saved from url=(0014)about:internet -->
+<html>
+<head>
+<style>
+/*begincss*/
+<cssfuzzer>
+/*endcss*/
+</style>
+<script>
+
+function freememory() {
+  try { CollectGarbage(); } catch(err) { }
+  try { FuzzingFunctions.garbageCollect(); } catch(err) { }
+  try { FuzzingFunctions.cycleCollect(); } catch(err) { }
+  try { window.gc(); } catch(err) { }
+}
+
+var runcount = {'jsfuzzer':0, 'eventhandler1':0, 'eventhandler2':0, 'eventhandler3':0, 'eventhandler4':0, 'eventhandler5':0}
+
+function GetVariable(fuzzervars, var_type) { if(fuzzervars[var_type]) { return fuzzervars[var_type]; } else { return null; }}
+
+function SetVariable(fuzzervars, var_name, var_type) { fuzzervars[var_type] = var_name; }
+
+function jsfuzzer() {
+
+runcount["jsfuzzer"]++; if(runcount["jsfuzzer"] > 2) { return; }
+
+<jsfuzzer>
+
+}
+
+function eventhandler1() {
+
+runcount["eventhandler1"]++; if(runcount["eventhandler1"] > 2) { return; }
+
+<jsfuzzer>
+
+}
+
+function eventhandler2() {
+
+runcount["eventhandler2"]++; if(runcount["eventhandler2"] > 2) { return; }
+
+<jsfuzzer>
+
+}
+
+function eventhandler3() {
+
+runcount["eventhandler3"]++; if(runcount["eventhandler3"] > 2) { return; }
+
+<jsfuzzer>
+
+}
+
+function eventhandler4() {
+
+runcount["eventhandler4"]++; if(runcount["eventhandler4"] > 2) { return; }
+
+<jsfuzzer>
+
+}
+
+function eventhandler5() {
+
+runcount["eventhandler5"]++; if(runcount["eventhandler5"] > 2) { return; }
+
+<jsfuzzer>
+
+}
+
+</script>
+</head>
+<body onload=jsfuzzer()>
+<!--beginhtml--><htmlfuzzer><!--endhtml-->
+</body>
+</html>
